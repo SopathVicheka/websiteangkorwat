318cceb7a112aa3af869e9ea8f5dbcddcb7dd98a
SopathVicheka <sopathvichhika@gmail.com>
2025-10-04 17:10:16 +0700

    test

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+alert ("សួស្ដី ខ្ញុំបាទឈ្មោះ សុផាត វិច្ឆិកា ។ រីករាយណាស់ដែលបានអ្នកទាំងអស់គ្នា​ ចូលមកកាន់វេបសាយ អាណាចក្រ អង្គរ សូមអរគុណ។");
